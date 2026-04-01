4c4cd148bcf1341037f230f6115abff31b796d7f
Viktor <167817135+vlovold@users.noreply.github.com>
2026-04-01 14:16:28 +0200

    Added templates dir for HTML files and made a simple app for testing

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello GitHub")
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/data")
+def data():
+    return {"sensor": 42}
+
+if __name__ == "__main__":
+    app.run(debug=True)
