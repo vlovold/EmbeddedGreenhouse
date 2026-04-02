0fd1891d50ddea6d43676dbb40d5aa99e2fd6acd
Viktor <167817135+vlovold@users.noreply.github.com>
2026-04-02 15:40:30 +0200

    Added more sensor values and did some work on the front end.

--- data.py
+++ data.py
@@ -1,11 +1,3 @@
 
-sensor_data = {
-    "temperature": 25,
-    "soil_humidity": 0.7,
-    "air_humidity": 1,
-    "brightness": 600,
-    "fan_speed": 400
-}
-
 def usart_read(data, sensor):
     return data[sensor]
--- main.py
+++ main.py
@@ -1,31 +1,28 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 from data import usart_read
 #testing data
 sensor_data = {
     "temperature": 24,
-    "soil_humidity": 0.7,
-    "air_humidity": 1,
+    "soil_humidity": 2,
+    "air_humidity": 4,
     "brightness": 600,
     "fan_speed": 400
 }
 
-
-
-
 app = Flask(__name__)
 @app.route("/")
 def index():
-    data_temperature = 30
-    return render_template("index.html",
-                           data_temp=data_temperature)
+    return render_template("index.html")
 
 @app.route("/data")
 def data():
     return {
-        "temp" : usart_read(sensor_data, "temperature"),
-        "soil humidity" : usart_read(sensor_data, "soil_humidity")
+        "temp": usart_read(sensor_data, "temperature"),
+        "soil": usart_read(sensor_data, "soil_humidity"),
+        "air": usart_read(sensor_data, "air_humidity"),
+        "brightness": usart_read(sensor_data, "brightness"),
+        "fanspeed": usart_read(sensor_data, "fan_speed")
     }
-
 
 if __name__ == "__main__":
     app.run(debug=True)
