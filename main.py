af53432c9145352241710330d018a83585089db5
Viktor <167817135+vlovold@users.noreply.github.com>
2026-04-02 13:57:10 +0200

    Added a data-collecting python program. Just for testing. Added a JS script which updates the value of the sensor values each second.

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 from flask import Flask, render_template, jsonify, request
+from data import usart_read
+#testing data
+sensor_data = {
+    "temperature": 24,
+    "soil_humidity": 0.7,
+    "air_humidity": 1,
+    "brightness": 600,
+    "fan_speed": 400
+}
+
+
+
 
 app = Flask(__name__)
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data_temperature = 30
+    return render_template("index.html",
+                           data_temp=data_temperature)
 
 @app.route("/data")
 def data():
-    return {"sensor": 42}
+    return {
+        "temp" : usart_read(sensor_data, "temperature"),
+        "soil humidity" : usart_read(sensor_data, "soil_humidity")
+    }
+
 
 if __name__ == "__main__":
     app.run(debug=True)
