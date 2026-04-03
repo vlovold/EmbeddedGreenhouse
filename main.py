1cf591a57baa74052c15a901978d115623a9433a
Viktor <167817135+vlovold@users.noreply.github.com>
2026-04-03 16:39:22 +0200

    Reverted to testing set-up and started adding input functionality with slider. Also reworked the design.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 from data import uart_read, get_data,uart_init
 
-ser = uart_init()
+#ser = uart_init()
 app = Flask(__name__)
-app.run(host="0.0.0.0", port=5000)
+#app.run(host="0.0.0.0", port=5000)
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route("/data")
 def data():
-    msg = uart_read(ser)
-    parsed_data = get_data(msg)
+#    msg = uart_read(ser)
+#    parsed_data = get_data(msg)
+    parsed_data = {
+        "TEMP": 20,
+        "HUM": 30,
+        "BRIGHT": 600,
+        "SOIL": 40,
+        "FAN": 800
+    }
     return parsed_data
 
 
