42f64e50b865e40610ae27fdf957f68a04ebfc25
Viktor <167817135+vlovold@users.noreply.github.com>
2026-04-06 15:19:52 +0200

    Started work on enabling automatic control. Continued work on printing message-structure.

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from data import uart_read, get_data,uart_init
 #ser = uart_init()
 app = Flask(__name__)
 #app.run(host="0.0.0.0", port=5000)
+actuators = {"pump": 0, "fan": 0, "led": 0}
+mode = "MANUAL"
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -24,9 +26,29 @@ def data():
 
 @app.route("/set", methods=["POST"])
 def send_actuator():
+    global actuators
+    global mode
+    if mode == "AUTO":
+        return {"status": "ignored (AUTO mode)"}
     data = request.get_json()
-    value = data["pump"]
-    print("Recieved value:", value)
+    if "pump" in data:
+        actuators["pump"] = data["pump"]
+    if "fan" in data:
+        actuators["fan"] = data["fan"]
+    if "led" in data:
+        actuators["led"] = data["led"]
+    print("LED:", actuators["led"], "PUMP:", actuators["pump"], "FAN:", actuators["fan"])
+    return {"status": "ok"}
+
+@app.route("/mode", methods=["POST"])
+def set_mode():
+    global mode
+
+    data = request.get_json()
+    mode = data["mode"]
+
+    print("Mode changed to:", mode)
+
     return {"status": "ok"}
 
 if __name__ == "__main__":
