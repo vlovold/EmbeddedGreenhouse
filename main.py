b9ca734c5c9659f053b72ddebba6dc689e9df92f
Viktor <167817135+vlovold@users.noreply.github.com>
2026-05-07 20:38:26 +0200

    added functionality for UART via USB

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ def video():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
