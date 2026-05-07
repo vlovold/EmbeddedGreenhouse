44fde1c3f1eb78908fc202394a55ac447142cdc3
Viktor <167817135+vlovold@users.noreply.github.com>
2026-05-07 20:25:36 +0200

    added functionality for UART via USB

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ mode = "MANUAL"
 @app.route("/")
 def index():
     return render_template("index.html")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
 @app.route("/data")
 def data():
